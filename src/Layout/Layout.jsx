fc426f5aad1ebf26b48e506504fea4ce47b59f83
abinandhan2003 <abinandhan77@gmail.com>
2024-12-12 10:12:13 +0530

    dkfmdkmfkds

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom'
 function Layout() {
   return (
     <div className='w-full flex justify-center items-center h-full'>
-      <div className='w-full md:w-96 h-[100vh] bg-[#2B223E] text-[#FCFCFC] py-10'>
+      <div className='w-full md:w-96 h-[90vh] bg-[#2B223E] text-[#FCFCFC] py-10'>
         
         <Outlet />
 
