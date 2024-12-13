73a7cc5b15026ee8aa50c62c47ba73eaa8c64615
abinandhan2003 <abinandhan77@gmail.com>
2024-12-13 12:28:57 +0530

    sdnvnfjndfkdsfd

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -5,7 +5,7 @@ import './Layout.css'
 function Layout() {
   return (
     <div className='w-full flex justify-center items-center'>
-      <div className='w-full md:w-96 h-[97vh] md:h-[100vh] bg-[#2B223E] text-[#FCFCFC] py-10'>
+      <div className='w-full md:w-96 h-[95vh] md:h-[100vh] bg-[#2B223E] text-[#FCFCFC] py-10'>
         
         <Outlet />
 
